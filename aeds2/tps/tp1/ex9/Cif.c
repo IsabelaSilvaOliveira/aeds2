7b4bfea87290c2e46a8349b5a745c1fc9c0a5c24
Isabela <isabelasilvaoliveiram2@gmail.com>
2026-08-27 20:37:41 -0300

    Update Cif.c

--- aeds2/tps/tp1/ex9/Cif.c
+++ aeds2/tps/tp1/ex9/Cif.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+// Cifra recursivamente cada caractere da string, fazendo o caracter deslocar 3 posicoes .
 void cifrar( char in [], int i) {
 
 	int n = 0;
@@ -19,16 +20,39 @@ int main () {
 
 	char f[100];
 	int n =0;
+    int i = 0;
 
-	scanf("%s",f);
+	fgets(f, 100, stdin);
+
+	
+    i = 0;
+    while (f[i] != '\0') {
+        if (f[i] == '\n') {
+            f[i] = '\0';
+        }
+        i++;
+    }
 
 
 	while(!(f[0] == 'F' && f[1] == 'I' && f[2] == 'M' && f[3] == '\0')){
 
 		cifrar(f,0);
 		printf("\n");
+		fgets(f, 100, stdin);
 
-		scanf("%s",f);
-
+		i = 0;
+        while (f[i] != '\0') {
+            if (f[i] == '\n') {
+                f[i] = '\0';
+            }
+            i++;
+        }
 	}
 }
+/*
+* abcde - edcba
+* arara - arara
+* a - a
+* abcd - dcba
+* obs: pensei que era em c++, entao tive que mudar o sacnf para o fgets, pois nele nao tava lendo uma frase completa.
+*/
