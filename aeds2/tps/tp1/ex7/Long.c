c1beb68239b5bf9cb7770283805f8a117083817b
Isabela <isabelasilvaoliveiram2@gmail.com>
2026-08-27 20:29:20 -0300

    Update Long.c

--- aeds2/tps/tp1/ex7/Long.c
+++ aeds2/tps/tp1/ex7/Long.c
@@ -1,55 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//calcula o comprimento da maior substring sem caracteres repetidos.
-int longa(char in[]){
-	int a = 1;
-	int c = 1; 
-	int b = 0;
-	int h =0;
-	int n =0;
-	int i =0;
-	int j =0;
+// Calcula o comprimento da maior substring sem caracteres repetidos.
+int longa(char in[]) {
+    int a = 1;
+    int b = 0;
+    int n = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int h = 0;
+    int resp = 0;
+    int repetiu = 0;
+    int continua = 1;
 
 
-	while(a){
-	if(in[b]){
-        n++;
-	}else
-	{ a = 1;}
-	b++;
-	}
-	
-	
-	  for(i = 0; i < n-1; i++) {
-		  for(j = i+1; j< n; j++) {
-			  if(in[i] == in[j] && c){
-				  c = 1;
-				  h = j;
+    while (a) {
+        if (in[b] != '\0') {
+            n++;
+        } else {
+            a = 0;
+        }
+        b++;
+    }
 
-		               }
-		 	         
-		   }
-	   }
-	 if( h == 0) {
-		 h = n;}
-	
-	//printf("%d\n",h);
+   
+    for (i = 0; i < n; i++) {
 
-	return h;
+        h = 0;
+        continua = 1;
+
+        for (j = i; j < n && continua; j++) {
+
+            repetiu = 0;
+
+            // Verifica se in[j] já apareceu entre i e j-1
+            for (k = i; k < j; k++) {
+                if (in[j] == in[k]) {
+                    repetiu = 1;
+                }
+            }
+
+            if (repetiu) {
+                continua = 0;
+            } else {
+                h++;
+
+                if (h > resp) {
+                    resp = h;
+                }
+            }
+        }
+    }
+
+    return resp;
 }
 
+int main() {
+    char f[100];
 
+    scanf("%s", f);
 
+    while (!(f[0] == 'F' &&
+             f[1] == 'I' &&
+             f[2] == 'M' &&
+             f[3] == '\0')) {
 
-int main () {
-
-	char f[100];
-	scanf("%s", f);
-
-	while(!(f[0] == 'F' && f[1] == 'I' && f[2] == 'M' && f[3] == '\0')) {
-	
         printf("%d\n", longa(f));
-	scanf("%s", f);
-	}
+
+        scanf("%s", f);
+    }
 }
+
+/*
+Testes:
+abcabcbb - 3
+abcc - 3
+abcdbe - 4
+pwwkew - 3
+abcdef - 6
+aaaa - 1
+a - 1
+obs: tive que adicionar o for que verifica se in[j] já apareceu entre i e j-1 pois eu estava achando apenas a substring antes de encontrar uma letra repetida, e nao a maior.
+*/
