3e047b33e5b50bf716928fceeefda67acfd8343c
Isabela <isabelasilvaoliveiram2@gmail.com>
2026-08-27 19:53:09 -0300

    Refactor anagrama function for clarity and correctness

--- aeds2/tps/tp1/ex6/Anag.c
+++ aeds2/tps/tp1/ex6/Anag.c
@@ -1,69 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool anagrama( char a[], char b[]){
-	bool resp = true;
-	bool z = true;
-	int i = 0;
-	int j = 0;
-	int t1 =0;
-	int t2 = 0;
-	int c = 0;
-	int y =0;
+// Verifica  se duas strings sao anagramas.
+int anagrama(char a[], char b[]) {
+    int resp = 1;
+    int z = 1;
+    int i = 0;
+    int j = 0;
+    int t1 = 0;
+    int t2 = 0;
+    int c = 0;
+    int y = 0;
 
-        while(z) {
-	if(a[c]){
-	t1++;
-	}else{
-	z = false;}
-	c++;
-	}
+    while (z) {
+        if (a[c] != '\0') {
+            t1++;
+        } else {
+            z = 0;
+        }
+        c++;
+    }
 
-	c = 0;
-	z = true;
+    c = 0;
+    z = 1;
 
-	while(z){
-	if(b[c]) {
-	t2++;
-	}else{
-	z = false;}
-	c++;
-	}
-	
-	if ( t1 != t2 ) {
-	resp = false;
-	}
-	else {
-	for(i = 0; i< t1; i++) {
-		for(j = 0; j< t1; j++) {
-			if( a[i] == b[j]){
-				y++;
-			}
-		}
-		if( y == 0 ){
-		resp = false;}
-	     }
-	}
+    while (z) {
+        if (b[c] != '\0') {
+            t2++;
+        } else {
+            z = 0;
+        }
+        c++;
+    }
 
-	return resp;
+    if (t1 != t2) {
+        resp = 0;
+    }
+    else {
+        for (i = 0; i < t1; i++) {
+
+            y = 0;
+
+            for (j = 0; j < t1; j++) {
+                if (a[i] == b[j]) {
+                    y++;
+                }
+            }
+
+            if (y == 0) {
+                resp = 0;
+            }
+        }
+    }
+
+    return resp;
 }
 
+int main() {
+    char a[100];
+    char b[100];
 
-int main () {
- char a[100];
- char b[100];
- 
+    scanf("%s", a);
+    scanf("%s", b);
 
- scanf("%s", a);
- scanf("%s", b);
+    while (!(a[0] == 'F' &&
+             a[1] == 'I' &&
+             a[2] == 'M' &&
+             a[3] == '\0')) {
 
- while(!(a[0] == 'F' && a[1] == 'I' && a[2] == 'M' && a[3] == '\0')) {
-	 if(anagrama(a,b)){
-           printf("SIM\n"); 
-	}else
-	{  printf("NAO\n");}
-	scanf("%s", a);
-	scanf("%s", b);
+        if (anagrama(a, b)) {
+            printf("SIM\n");
+        }
+        else {
+            printf("NAO\n");
+        }
+
+        scanf("%s", a);
+        scanf("%s", b);
+    }
 }
 
-}
+/*
+testes:
+* listen silent - SIM
+* amor roma - SIM
+* abc abd - NAO
+* abc ab - NAO
+*/
